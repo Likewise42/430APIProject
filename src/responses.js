a4c8fec9815293cc95e09d2eadc9c9e1e0b66ca3
Ryan Muskopf <ryanjmuskopf@gmail.com>
2017-03-08 02:27:51 -0500

    you can now upload image links

--- src/responses.js
+++ src/responses.js
@@ -1,10 +1,105 @@
+// ryan muskopf http assignment 2
 const fs = require('fs');
 const crypto = require('crypto');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const style = fs.readFileSync(`${__dirname}/../client/style.css`);
-
-const users = {};
-
-let etag = crypto.createHash('sha1').update(JSON.stringify(users));
+const images = {};
+let etag = crypto.createHash('sha1').update(JSON.stringify(images));
 let digest = etag.digest('hex');
+
+// ryan muskopf http assignment 2
+// index and css
+const getIndex = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+
+  response.write(index);
+
+  response.end();
+};
+module.exports.getIndex = getIndex;
+
+const getStyle = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/css' });
+
+  response.write(style);
+
+  response.end();
+};
+module.exports.getStyle = getStyle;
+
+// ryan muskopf http assignment 2
+// responds
+const respond = (request, response, status, content) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    etag: digest,
+  };
+
+  console.dir(content);
+
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(content));
+  response.end();
+};
+
+const respondMeta = (request, response, status) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    etag: digest,
+  };
+
+  response.writeHead(status, headers);
+  response.end();
+};
+
+// ryan muskopf http assignment 2
+// post
+const upload = (request, response, body) => {
+  const responseJSON = {
+    message: 'need title, link, and author',
+  };
+
+  console.dir(body);
+
+  if (!body.link || !body.title || !body.author) {
+    responseJSON.id = 'missing params';
+    return respond(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 201;
+
+  if (images[body.name]) {
+    responseCode = 204;
+  } else {
+    images[body.name] = {};
+  }
+
+  images[body.name].name = body.name;
+  images[body.name].age = body.age;
+
+  etag = crypto.createHash('sha1').update(JSON.stringify(images));
+  digest = etag.digest('hex');
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Uploaded Image Data Successfully';
+    return respond(request, response, responseCode, responseJSON);
+  }
+
+  return respondMeta(request, response, responseCode);
+};
+module.exports.upload = upload;
+
+// ryan muskopf http assignment 2
+// not head and 304
+const notFound = (request, response, params, type) => {
+  const responseJSON = {
+    message: 'The page was not found',
+    id: 'notFound',
+  };
+  respond(request, response, 404, responseJSON, type);
+};
+module.exports.notFound = notFound;
+
+
+// 304 and head
--- src/server.js
+++ src/server.js
@@ -1,20 +1,65 @@
+ // ryan muskopf http assignment 2
 const http = require('http');
 const url = require('url');
 const query = require('querystring');
-
 // handelers
 const responseHandler = require('./responses.js');
-
 // port
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+// ryan muskopf http assignment 2
+// put url stuff here
+const onRequest = (request, response) => {
+  // parse the url
+  const parsedUrl = url.parse(request.url);
 
-//put url stuff here
+  if (request.method === 'GET') {
+    if (parsedUrl.pathname === '/') {
+      responseHandler.getIndex(request, response);
+    } else if (parsedUrl.pathname === '/style.css') {
+      responseHandler.getStyle(request, response);
+    // } else if (parsedUrl.pathname === '/') {
+      // responseHandler.getU(request, response);
+    } else {
+      responseHandler.notFound(request, response);
+    }
+  } else if (request.method === 'HEAD') {
+    // if (parsedUrl.pathname === '/') {
+    //  responseHandler.getUsersMeta(request, response);
+    // } else {
+    responseHandler.notFoundMeta(request, response);
+    // }
+  } else if (request.method === 'POST' && parsedUrl.pathname === '/upload') {
+    const res = response;
+
+    const body = [];
+
+    request.on('error', () => {
+      res.statusCode = 400;
+      res.end();
+    });
 
 
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on('end', () => {
+      console.log(Buffer.concat(body).toString());
 
 
+      const bodyString = Buffer.concat(body).toString();
+
+      const bodyParams = query.parse(bodyString);
+
+      responseHandler.upload(request, res, bodyParams);
+    });
+  } else {
+    responseHandler.notFound(request, response);
+  }
+};
+
+// ryan muskopf http assignment 2
 // start HTTP server
 http.createServer(onRequest).listen(port);
-
 console.log(`Listening on 127.0.0.1: ${port}`);
